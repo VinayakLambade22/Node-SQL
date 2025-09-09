9e71e905244418b0f723fa004d241c2c4816dd61
Vinayak Lambade <vinayaklambade66593@gmail.com>
2025-09-09 15:43:38 +0530

    Add styling to all EJS views

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
 const app = express();
 const path = require("path");
@@ -11,91 +10,34 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-let getUser = () => {
-  return [
-    faker.datatype.uuid(),
-    faker.internet.userName(),
-    faker.internet.email(),
-    faker.internet.password(),
-  ];
-};
-
 let connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "mysql@123",
+  password: "vinayak",
   database: "app",
 });
 
 app.get("/", (req, res) => {
-  let q = `SELECT count(*) FROM user`;
-  try {
-    connection.query(q, (err, result) => {
-      if (err) throw err;
-      let count = result[0]["count(*)"];
-      res.render("home.ejs", { count });
-    });
-  } catch (err) {
-    res.send("some error occurred");
-  }
+  let q = `SELECT count(*) as count FROM user`;
+  connection.query(q, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database query failed.");
+    }
+    let count = result[0].count;
+    res.render("home.ejs", { count });
+  });
 });
 
 app.get("/user", (req, res) => {
   let q = `SELECT * FROM user`;
-  try {
-    connection.query(q, (err, result) => {
-      if (err) throw err;
-      let data = result;
-      res.render("users.ejs", { data });
-    });
-  } catch (err) {
-    res.send("some error occurred");
-  }
-});
-
-app.get("/user/:id/edit", (req, res) => {
-  let { id } = req.params;
-  let q = `SELECT * FROM user WHERE id='${id}'`;
-
-  try {
-    connection.query(q, (err, result) => {
-      if (err) throw err;
-      let user = result[0];
-      res.render("edit.ejs", { user });
-    });
-  } catch (err) {
-    res.send("some error with DB");
-  }
-});
-
-app.patch("/user/:id", (req, res) => {
-  let { id } = req.params;
-  let { username, password } = req.body;
-  console.log(username);
-  let q = `SELECT * FROM user WHERE id='${id}'`;
-
-  try {
-    connection.query(q, (err, result) => {
-      if (err) throw err;
-      let user = result[0];
-
-      if (user.password != password) {
-        res.send("WRONG Password entered!");
-      } else {
-        let q2 = `UPDATE user SET username='${username}' WHERE id='${id}'`;
-        connection.query(q2, (err, result) => {
-          if (err) throw err;
-          else {
-            console.log(result);
-            console.log("updated!");
-            res.redirect("/user");
-          }
-        });
-      }
-    });
-  } catch (err) {
-    res.send("some error with DB");
-  }
+  connection.query(q, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database query failed.");
+    }
+    res.render("users.ejs", { data });
+  });
 });
 
 app.get("/user/new", (req, res) => {
@@ -105,64 +47,115 @@ app.get("/user/new", (req, res) => {
 app.post("/user/new", (req, res) => {
   let { username, email, password } = req.body;
   let id = uuidv4();
-  //Query to Insert New User
-  let q = `INSERT INTO user (id, username, email, password) VALUES ('${id}','${username}','${email}','${password}') `;
+  let q = `INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?)`;
+  let values = [id, username, email, password];
 
-  try {
-    connection.query(q, (err, result) => {
-      if (err) throw err;
-      console.log("added new user");
-      res.redirect("/user");
-    });
-  } catch (err) {
-    res.send("some error occurred");
-  }
+  connection.query(q, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Failed to add new user.");
+    }
+    console.log("Added new user");
+    res.redirect("/user");
+  });
+});
+
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM user WHERE id = ?`;
+
+  connection.query(q, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database query failed.");
+    }
+    if (result.length === 0) {
+      return res.status(404).send("User not found.");
+    }
+    let user = result[0];
+    res.render("edit.ejs", { user });
+  });
+});
+
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let { username, password } = req.body;
+  let qSelect = `SELECT * FROM user WHERE id = ?`;
+
+  connection.query(qSelect, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database query failed.");
+    }
+    if (result.length === 0) {
+      return res.status(404).send("User not found.");
+    }
+    let user = result[0];
+
+    if (user.password !== password) {
+      res.send("WRONG Password entered!");
+    } else {
+      let qUpdate = `UPDATE user SET username = ? WHERE id = ?`;
+      connection.query(qUpdate, [username, id], (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Failed to update user.");
+        }
+        console.log("Updated!");
+        res.redirect("/user");
+      });
+    }
+  });
 });
 
 app.get("/user/:id/delete", (req, res) => {
   let { id } = req.params;
-  let q = `SELECT * FROM user WHERE id='${id}'`;
-
-  try {
-    connection.query(q, (err, result) => {
-      if (err) throw err;
-      let user = result[0];
-      res.render("delete.ejs", { user });
-    });
-  } catch (err) {
-    res.send("some error with DB");
-  }
+  let q = `SELECT * FROM user WHERE id = ?`;
+  connection.query(q, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database query failed.");
+    }
+    if (result.length === 0) {
+      return res.status(404).send("User not found.");
+    }
+    let user = result[0];
+    res.render("delete.ejs", { user });
+  });
 });
 
-app.delete("/user/:id/", (req, res) => {
+// Delete user route
+app.delete("/user/:id", (req, res) => {
   let { id } = req.params;
   let { password } = req.body;
-  let q = `SELECT * FROM user WHERE id='${id}'`;
+  let qSelect = `SELECT * FROM user WHERE id = ?`;
 
-  try {
-    connection.query(q, (err, result) => {
-      if (err) throw err;
-      let user = result[0];
+  connection.query(qSelect, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database query failed.");
+    }
+    if (result.length === 0) {
+      return res.status(404).send("User not found.");
+    }
+    let user = result[0];
 
-      if (user.password != password) {
-        res.send("WRONG Password entered!");
-      } else {
-        let q2 = `DELETE FROM user WHERE id='${id}'`; //Query to Delete
-        connection.query(q2, (err, result) => {
-          if (err) throw err;
-          else {
-            console.log(result);
-            console.log("deleted!");
-            res.redirect("/user");
-          }
-        });
-      }
-    });
-  } catch (err) {
-    res.send("some error with DB");
-  }
+    if (user.password !== password) {
+      res.send("WRONG Password entered!");
+    } else {
+      let qDelete = `DELETE FROM user WHERE id = ?`;
+      connection.query(qDelete, [id], (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Failed to delete user.");
+        }
+        console.log("Deleted!");
+        res.redirect("/user");
+      });
+    }
+  });
 });
 
 app.listen("8080", () => {
-  console.log("server running on port 8080");
+  console.log("Server running on port 8080");
 });
